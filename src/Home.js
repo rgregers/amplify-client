16eae8f3a9e5eb85b13d78c6517fa0f97cb75a22
Tony Li Laptop <litoj2001@gmail.com>
2022-11-10 11:06:08 -0800

    base structure and states, handlesubmit in progress

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import axios from "axios";
 function Home() {
   const [className, setClassName] = useState("");
 
-  const [fileInput, setFileInput] = useState("");
+  const [seqs, setSeqs] = useState("");
 
-  const [result, setResult] = useState("");
+  const [headers, setHeaders] = useState("");
 
   useEffect(() => {
     getClassName().then(result => {
@@ -29,8 +29,39 @@ function Home() {
   
   function handleSubmit(event) {
     event.preventDefault()
+    var file = document.getElementById("myFile").files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      // read fasta
+      var sequences = readFunction(e.target.result)
+      console.log(e.target.result)
+    };
+    reader.readAsText(file);
     return
     // read fasta file
+  }
+
+  function readFunction(input) {
+    var text = input.split("\n")
+    var seqs = []
+    var headers = []
+    for (let i = 0; i < text.length; i++) {
+      if (text[i][0] == '>') {
+        headers.push(text[i].trim())
+      }
+      else {
+        seqs.push(text[i].trim())
+      }
+    }
+    console.log(headers)
+    console.log(seqs)
+    return seqs
+  }
+
+  function runScraper() {
+    var motif = document.getElementById("motif").target.result
+    //run motif scraper using motif variable using the python-shell
+    return
   }
 
   return(
@@ -38,12 +69,12 @@ function Home() {
       <h1>Hello {className.data}!</h1>
       <form onSubmit={handleSubmit}>
           <h5>Fasta File Upload</h5>
-          <input type="file" />
-          <button type="submit" name="upload">Upload</button>
+          <input id="myFile" type="file"/>
+          <button type="submit" name="upload">Run Scraper</button>
         </form>
       <h5>Motif Here</h5>
       <textarea rows={3} cols={30} id="motif"></textarea>
-      <button>Run Scraper</button>
+
 
     </div>
   );
