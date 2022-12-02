82b28e725a76570a6fcc7e165a470e3db10077e1
Tony Li Laptop <litoj2001@gmail.com>
2022-12-01 19:40:30 -0800

    last changes for presentation

--- src/Home.js
+++ src/Home.js
@@ -15,50 +15,24 @@ function Home() {
     console.log("hiding r2dt")
     return false;
   }
-  const [className, setClassName] = useState("");
 
-  const [seqs, setSeqs] = useState([]);
-
-  const [heads, setHeaders] = useState([]);
-
-  useEffect(() => {
-    getClassName().then(result => {
-      if (result && result.status == 201) {
-        setClassName(result);
-      }
-    });
-  }, []);
-
-  const getClassName = async () => {
-    try {
-      const response = await axios.get("http://localhost:5001/");
-      return response;
-    }
-    catch (error) {
-      console.log("Error: " + error.message);
-      return false;
-    }
-  }
   function handleAll(event) {
     handleSubmit(event)
-    checkStates()
   }
   function handleSubmit(event) {
     event.preventDefault()
     var file = document.getElementById("myFile").files[0];
     var reader = new FileReader();
+    var motif = document.getElementById("motif").value
     reader.onload = function (e) {
       // read fasta
       var [headers, sequences] = readFunction(e.target.result)
-      runScraper()
-      console.log(e.target.result)
-      document.getElementById("resultarea").value = "motif used: " + document.getElementById("motif").value + "\nmotif found: False/True" + "\nAt indices: x, y, z"
-      setHeaders(headers)
-      setSeqs(sequences)
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("motif", motif);
+      runScraper(formData)
+      //Sends event trigger to modify text area
       handleTextUpdate(headers, sequences)
-      
-      // buttonOne.disabled=false
-      // buttonTwo.disabled=false
     };
     reader.readAsText(file);
     return
@@ -88,11 +62,6 @@ function Home() {
     r2dtTextArea.dispatchEvent(event);
   }
 
-  function checkStates() {
-    console.log(heads)
-    console.log(seqs)
-  }
-
   function readFunction(input) {
     var text = input.split("\n")
     var seqs = []
@@ -120,48 +89,97 @@ function Home() {
     return [headers, seqs]
   }
 
-  function runScraper() {
-    var motif = document.getElementById("motif").value
-    //run motif scraper using motif variable using the python-shell
-    console.log("This is the motif found in the -motif- text box element: ", motif)
+  // function runScraper(formData) {
+  //   var motif = document.getElementById("motif").value
+  //   //run motif scraper using motif variable using the python-shell
+  //   console.log("This is the motif found in the -motif- text box element: ", motif)
 
-    //result contains an example output running motifscraper on
-    // >KI270394.1 dna_sm:scaffold scaffold:GRCh38:KI270394.1:1:970:1 REF
-    // aagtggatatttggatagctttgaggatttcgttggaaacgggattacatataaaatcta
-    // gagagaagcattctcaggaacttctttgtgatgtttgcattcaagtcacagaactgaaca
-    // ttccctttcatagagcaggtttgaaacactctttctgtagtatctgcaaacggacatttc
-    // atacgctttcaggcctatggtgagaaaggaaatatcttcaaataaaaactagacagaagc
-    // attctcagaaacttatttgcgatgtgtgtcctcaactaacagagttgaacctttgttttg
-    // atacagcattttggaaacactctttttgtaggatctgcaggtggatatttggataggttt
-    // gaaggtttcgttggaaacgggaatatcttcatataaaatcaacacagaagcattctcaga
-    // aacttctctgcgatgtttgcattcaactcatagagttgaacacttcctttcatagagctg
-    // gtttgaaatactctttttgtaatatttggaagtggacattggcagcgctttgaagcctat
-    // gttgaaaatggaaatatcttctcctaaaaaccagacagaagcattctcagaaacttcctt
-    // gtgatgtgtgtactcaagtaacagagttgaaccttacttttgacagagccgttttgaaac
-    // agtctttttgtagaatctggaagtagatatttggatacctttgaggatttctttggaaac
-    // gggatatcttcatataaaatctagacagaagcattctcagaaacttctttgtgctgtatg
-    // tcctcaattaacagagttgaacctttgtgtggatacagcattttggaaacactcctttag
-    // tagaatctgcaagttgatacttagataggaagatttccttggaaacgggaatatcttcat
-    // ataaaatctagacggaagcattctcggaaacttctttgtgctgtatgtcctcaataacag
-    // agttgaacct
-    //looking for TTTGCA
-    var result = "Contig,Start,End,Strand,Sequence,Motif\nKI270394.1,94,99,+,TTTGCA,TTTGCA\nKI270394.1,436,441,+,TTTGCA,TTTGCA\nKI270394.1,170,165,-,TTTGCA,TTTGCA"
+  //   //result contains an example output running motifscraper on
+  //   // >KI270394.1 dna_sm:scaffold scaffold:GRCh38:KI270394.1:1:970:1 REF
+  //   // aagtggatatttggatagctttgaggatttcgttggaaacgggattacatataaaatcta
+  //   // gagagaagcattctcaggaacttctttgtgatgtttgcattcaagtcacagaactgaaca
+  //   // ttccctttcatagagcaggtttgaaacactctttctgtagtatctgcaaacggacatttc
+  //   // atacgctttcaggcctatggtgagaaaggaaatatcttcaaataaaaactagacagaagc
+  //   // attctcagaaacttatttgcgatgtgtgtcctcaactaacagagttgaacctttgttttg
+  //   // atacagcattttggaaacactctttttgtaggatctgcaggtggatatttggataggttt
+  //   // gaaggtttcgttggaaacgggaatatcttcatataaaatcaacacagaagcattctcaga
+  //   // aacttctctgcgatgtttgcattcaactcatagagttgaacacttcctttcatagagctg
+  //   // gtttgaaatactctttttgtaatatttggaagtggacattggcagcgctttgaagcctat
+  //   // gttgaaaatggaaatatcttctcctaaaaaccagacagaagcattctcagaaacttcctt
+  //   // gtgatgtgtgtactcaagtaacagagttgaaccttacttttgacagagccgttttgaaac
+  //   // agtctttttgtagaatctggaagtagatatttggatacctttgaggatttctttggaaac
+  //   // gggatatcttcatataaaatctagacagaagcattctcagaaacttctttgtgctgtatg
+  //   // tcctcaattaacagagttgaacctttgtgtggatacagcattttggaaacactcctttag
+  //   // tagaatctgcaagttgatacttagataggaagatttccttggaaacgggaatatcttcat
+  //   // ataaaatctagacggaagcattctcggaaacttctttgtgctgtatgtcctcaataacag
+  //   // agttgaacct
+  //   //looking for TTTGCA
+  //   var result = "Contig,Start,End,Strand,Sequence,Motif\nKI270394.1,94,99,+,TTTGCA,TTTGCA\nKI270394.1,436,441,+,TTTGCA,TTTGCA\nKI270394.1,170,165,-,TTTGCA,TTTGCA"
     
-    var text = result.split("\n")
-    console.table(text)
-    if (text.length == 1) {
-      //the motif did not appear in the given sequence
-      //output picture of RNA sequence structure
-      //    using R2DT
-      console.log("No motif occurrence found")
+  //   var text = result.split("\n")
+  //   console.table(text)
+  //   if (text.length == 1) {
+  //     //the motif did not appear in the given sequence
+  //     //output picture of RNA sequence structure
+  //     //    using R2DT
+  //     console.log("No motif occurrence found")
+  //   }
+  //   else if (text.length > 1) {
+  //     //the sequence contains 1 or more occurrences of the motif
+  //     //output picture of RNA sequence structure
+  //     //    using R2DT
+  //     console.log("Motif occurred: ", text.length - 1, " times")
+  //     for(var i=1; i < (text.length); i++) {
+  //       var info = text[i].split(",")
+  //       var contig = info[0]
+  //       var start = info[1]
+  //       var end = info[2]
+  //       console.log("Occuring in contig: ", contig, " between ", start, " and ", end)
+  //     }
+  //   }
+  //   return
+  // }
+  
+  async function runScraper(formData) {
+    var response = null;
+    try {
+      response = await axios.post("http://localhost:5001/", formData, {headers: {"Content-Type": "multipart/form-data"}});
     }
-    else if (text.length > 1) {
-      //the sequence contains 1 or more occurrences of the motif
-      //output picture of RNA sequence structure
-      //    using R2DT
-      console.log("Motif occurred: ", text.length - 1, " times")
+    catch(error) {
+      console.log(error);
+      return false;
     }
+    console.log(response);
+    var results = response.data[0]
+    var sequence = results[0]
+    var indices = results[1]
+    var header = results[2]
+
+    var stringResult = header + "\n"
+    for(var i=0; i < indices.length; i++) {
+      var start = indices[i]
+      var end = start + formData.get("motif").length
+      stringResult += "Occurs between " + start + " and " + end + "\n"
+
+    }
+    // console.log(e.target.result)
+    document.getElementById("resultarea").value = "motif used: " + document.getElementById("motif").value + "\n" + stringResult
+    console.log(stringResult)
     return
+    // var result = "Contig,Start,End,Strand,Sequence,Motif\nKI270394.1,94,99,+,TTTGCA,TTTGCA\nKI270394.1,436,441,+,TTTGCA,TTTGCA\nKI270394.1,170,165,-,TTTGCA,TTTGCA"
+    
+    // var text = result.split("\n")
+    // if (text.length == 1) {
+    //   //the motif did not appear in the given sequence
+    //   //output picture of RNA sequence structure
+    //   //    using R2DT
+    // }
+    // else if (text.length > 1) {
+    //   //the sequence contains 1 or more occurrences of the motif
+    //   //output picture of RNA sequence structure
+    //   //    using R2DT
+    // }
+
   }
 
   return(
@@ -193,7 +211,7 @@ function Home() {
         </div>
         <div className= "section2">
           <h5>Result</h5>
-          <textarea rows={5} cols={60} disabled id="resultarea"></textarea>
+          <textarea rows={5} cols={95} disabled id="resultarea"></textarea>
 
         </div>
       </div>
